a8b4d696e3d5460083bd9b03f5c43536ed6a7026
Vinicius Frota <viniciuspxfrota@gmail.com>
2021-10-05 01:21:33 -0300

    add Account, Expense and Income models
    Signed-off-by: Vinicius Frota <viniciuspxfrota@gmail.com>

--- moneymanagerapp/models.py
+++ moneymanagerapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+  bank_name = models.CharField()
+  currency = models.CharField()
+  amount = models.DecimalField(decimal_places=2)
+
+class Expense(models.Model):
+  value = models.DecimalField(decimal_places=2)
+  date = models.DateTimeField()
+
+class Income(models.Model):
+  value = models.DecimalField(decimal_places=2)
+  date = models.DateTimeField()
+
