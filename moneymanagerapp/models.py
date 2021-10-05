5180d833e0cb28565cbefca4f3c346a0cb936661
Vinicius Frota <viniciuspxfrota@gmail.com>
2021-10-05 13:10:10 -0300

    fix Models add models to admin add moneymanagerapp to installed apps
    Signed-off-by: Vinicius Frota <viniciuspxfrota@gmail.com>

--- moneymanagerapp/models.py
+++ moneymanagerapp/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 
 class Account(models.Model):
-  bank_name = models.CharField()
-  currency = models.CharField()
-  amount = models.DecimalField(decimal_places=2)
+  bank_name = models.CharField(max_length = 100)
+  currency = models.CharField(max_length = 100)
+  amount = models.DecimalField(max_digits= 1000000, decimal_places = 2)
 
 class Expense(models.Model):
-  value = models.DecimalField(decimal_places=2)
+  value = models.DecimalField(max_digits= 1000000, decimal_places = 2)
   date = models.DateTimeField()
+  account = models.ForeignKey(Account, on_delete = models.CASCADE)
 
 class Income(models.Model):
-  value = models.DecimalField(decimal_places=2)
+  value = models.DecimalField(max_digits= 1000000, decimal_places = 2)
   date = models.DateTimeField()
+  account = models.ForeignKey(Account, on_delete = models.CASCADE)
 
